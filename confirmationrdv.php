837734b4c396d6859551e97f44094a6d35713c18
MarionCassoret <marioncassoret26@gmail.com>
2017-05-09 17:13:23 +0200

    page confirmation de rdv

---
+++ confirmationrdv.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<meta charset="utf-8"> 
+		<body bgcolor="#B0C4DE">
+		<title> Mon rendez-vous</title>
+	</head>
+  	 <body>
+  	 	 <center><img src="bannierespa.png" ></center>
+
+  	 	 <center><h1> Nous vous remercions d'avoir pris rendez-vous dans notre institut Spa des îles, à très bientôt ! </h1></center>
+	</body>
+</html>
