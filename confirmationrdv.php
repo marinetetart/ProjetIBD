44b9dd2dbdb9b478c83c7b5d0927e2d87d46379a
MarionCassoret <marioncassoret26@gmail.com>
2017-05-12 18:45:20 +0200

    modification confirmation

--- confirmationrdv.php
+++ confirmationrdv.php
@@ -8,5 +8,7 @@
   	 	 <center><img src="bannierespa.png" ></center>
 
   	 	 <center><h1> Nous vous remercions d'avoir pris rendez-vous dans notre institut Spa des îles, à très bientôt ! </h1></center>
+  	 	 <center><h3> Nous allons traiter votre demande dans les plus brefs délais, vérifiez votre boîte mail nous allons vous faire parvenir votre rendez-vous. </h3></center>
+  	 	 <center><h3>A très bientôt dans notre institut Spa des îles.</h3></center>
 	</body>
 </html>
